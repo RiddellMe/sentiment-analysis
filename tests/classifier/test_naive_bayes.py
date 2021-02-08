fa8e67cd5102602bb5aabd2643a0756ce441c083
RiddellMe <red.riddell@gmail.com>
2021-02-08 21:45:30 +1100

    added training data, naive bayes is functional - need to add statistics and confusion matrix etc
    updated naive bayes logging

--- tests/classifier/test_naive_bayes.py
+++ tests/classifier/test_naive_bayes.py
@@ -2,11 +2,11 @@ from classifier.naive_bayes import NaiveBayes
 
 
 def test_naive_bayes_classifies_correctly(classification_data):
-    training_data, test_data, expected_output, list_output = classification_data
-    naive_bayes = NaiveBayes()
+    ticker, training_data, test_data, expected_output, list_output = classification_data
+    naive_bayes = NaiveBayes(words_to_ignore=[ticker])
     naive_bayes.train(training_data)
     actual_output = naive_bayes.test(test_data)
     actual_list_output = naive_bayes.convert_to_list()
     # convert to frozenset so assertion order does not matter (since we use concurrency)
     assert {frozenset(item) for item in actual_output} == {frozenset(item) for item in expected_output}
-    assert sorted(list_output) == sorted(actual_list_output)
+    assert sorted(actual_list_output) == sorted(list_output)
